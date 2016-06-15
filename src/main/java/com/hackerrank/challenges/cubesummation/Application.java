e85492ab9debfd315747e6e1cfd64595c8bfe129
John <john.19col@gmail.com>
2016-06-14 22:58:33 -0500

    create application class with main method and sample file with sample data

---
+++ src/main/java/com/hackerrank/challenges/cubesummation/Application.java
@@ -0,0 +1,27 @@
+package com.hackerrank.challenges.cubesummation;
+
+import com.hackerrank.challenges.cubesummation.executor.ResultListExecutor;
+import com.hackerrank.challenges.cubesummation.input.data.InputData;
+import com.hackerrank.challenges.cubesummation.input.parser.InputParser;
+import com.hackerrank.challenges.cubesummation.input.parser.StandardParser;
+import com.hackerrank.challenges.cubesummation.input.reader.FileReader;
+import com.hackerrank.challenges.cubesummation.input.reader.InputReader;
+import com.hackerrank.challenges.cubesummation.input.reader.InputReaderException;
+
+import java.util.List;
+
+public class Application {
+
+    private static final String TEST_FILE = "src/main/resources/inputs/sample-1.txt";
+
+    public static void main(String[] args) throws InputReaderException {
+        InputParser parser = new StandardParser();
+        InputReader reader = new FileReader(parser, TEST_FILE);
+        ResultListExecutor executor = new ResultListExecutor();
+        InputData data = reader.read();
+        List<Integer> results = executor.execute(data.getTestCases());
+        results.forEach(System.out::println);
+    }
+
+}
+
