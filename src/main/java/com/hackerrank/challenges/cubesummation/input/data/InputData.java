aad648276718bd9610bd350c04d934c0eccc60ad
John <john.19col@gmail.com>
2016-06-14 22:54:10 -0500

    created InputData to represent input

---
+++ src/main/java/com/hackerrank/challenges/cubesummation/input/data/InputData.java
@@ -0,0 +1,36 @@
+package com.hackerrank.challenges.cubesummation.input.data;
+
+import com.hackerrank.challenges.cubesummation.input.data.cases.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputData {
+
+    private final List<TestCase> testCases;
+
+    public InputData(List<TestCase> testCases) {
+        this.testCases = testCases;
+    }
+
+    public List<TestCase> getTestCases() {
+        return testCases;
+    }
+
+    public static class Builder {
+
+        private List<TestCase> testCases = new ArrayList<>();
+
+        public Builder() { }
+
+        public Builder withTestCase(TestCase testCase) {
+            testCases.add(testCase);
+            return this;
+        }
+
+        public InputData build() {
+            return new InputData(testCases);
+        }
+    }
+
+}
---
+++ src/main/java/com/hackerrank/challenges/cubesummation/input/data/cases/TestCase.java
@@ -0,0 +1,48 @@
+package com.hackerrank.challenges.cubesummation.input.data.cases;
+
+import com.hackerrank.challenges.cubesummation.input.data.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCase {
+
+    private final int matrixSize;
+    private final List<Query> queries;
+
+    public TestCase(int matrixSize, List<Query> queries) {
+        this.matrixSize = matrixSize;
+        this.queries = queries;
+    }
+
+    public int getMatrixSize() {
+        return matrixSize;
+    }
+
+    public List<Query> getQueries() {
+        return queries;
+    }
+
+    public static class Builder {
+
+        private int matrixSize;
+        private List<Query> queries = new ArrayList<>();
+
+        public Builder() {}
+
+        public Builder withMatrixSize(int matrixSize) {
+            this.matrixSize = matrixSize;
+            return this;
+        }
+
+        public Builder withQuery(Query query) {
+            this.queries.add(query);
+            return this;
+        }
+
+        public TestCase build() {
+            return new TestCase(matrixSize, queries);
+        }
+    }
+
+}
