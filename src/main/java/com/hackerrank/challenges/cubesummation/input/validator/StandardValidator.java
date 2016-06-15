72daa1caa03bc643cc25719280e32409c3691c61
John <john.19col@gmail.com>
2016-06-15 09:54:56 -0500

    added throws clause to private methods in standard validator

--- src/main/java/com/hackerrank/challenges/cubesummation/input/validator/StandardValidator.java
+++ src/main/java/com/hackerrank/challenges/cubesummation/input/validator/StandardValidator.java
@@ -19,25 +19,25 @@ public class StandardValidator implements InputValidator {
         });
     }
 
-    private static void validateNumberOfTestCases(InputData data) {
+    private static void validateNumberOfTestCases(InputData data) throws IllegalArgumentException {
         if (data.getNumberOfTestCases() < 1 || data.getNumberOfTestCases() > 50) {
             throw new IllegalArgumentException("Constrain: 1 <= T <= 50");
         }
     }
 
-    private static void validateMatrixSize(TestCase testCase) {
+    private static void validateMatrixSize(TestCase testCase) throws IllegalArgumentException {
         if (testCase.getMatrixSize() < 1 || testCase.getMatrixSize() > 100) {
             throw new IllegalArgumentException("Constrain: 1 <= N <= 100");
         }
     }
 
-    private static void validateNumberOfQueries(TestCase testCase) {
+    private static void validateNumberOfQueries(TestCase testCase) throws IllegalArgumentException {
         if (testCase.getNumberOfQueries() < 1 || testCase.getNumberOfQueries() > 1000) {
             throw new IllegalArgumentException("Constrain: 1 <= M <= 1000");
         }
     }
 
-    private static void validateUpdateQueries(TestCase testCase) {
+    private static void validateUpdateQueries(TestCase testCase) throws IllegalArgumentException {
         testCase.getQueries().stream()
                 .filter(q -> q instanceof UpdateQuery)
                 .map(q -> (UpdateQuery) q)
@@ -50,7 +50,7 @@ public class StandardValidator implements InputValidator {
                 });
     }
 
-    private static void validateReadQueries(TestCase testCase) {
+    private static void validateReadQueries(TestCase testCase) throws IllegalArgumentException {
         int matrixSize = testCase.getMatrixSize();
         testCase.getQueries().stream()
                 .filter(q -> q instanceof ReadQuery)
@@ -64,20 +64,20 @@ public class StandardValidator implements InputValidator {
                 });
     }
 
-    private static void validateCoordinate(TestCase testCase, int coordinate) {
+    private static void validateCoordinate(TestCase testCase, int coordinate) throws IllegalArgumentException {
         if (coordinate < 1 || coordinate > testCase.getMatrixSize()) {
             throw new IllegalArgumentException("Constrain: 1 <= x,y,z <= N");
         }
     }
 
-    private static void validateUpdateValue(UpdateQuery query) {
+    private static void validateUpdateValue(UpdateQuery query) throws IllegalArgumentException {
         if (query.getValue() < -10e9 || query.getValue() > 10e9) {
             throw new IllegalArgumentException("Constrain: -10e9 <= W <= 10e9");
         }
     }
 
-    private static void validateCoordinates(int coordinate1, int coordinate2, int matrixSize) {
-        if (!(1 <= coordinate1 && coordinate1 <= coordinate2 && coordinate2 <= matrixSize)) {
+    private static void validateCoordinates(int v1, int v2, int matrixSize) throws IllegalArgumentException {
+        if (!(1 <= v1 && v1 <= v2 && v2 <= matrixSize)) {
             throw new IllegalArgumentException("Constrain: 1 <= x1,y1,z1 <= x2,y2,z2 <= N");
         }
     }
